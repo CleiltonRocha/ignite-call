28772a87d6747f799299c017e3d0879375d62fa8
Cleilton Rocha <cleiltonrv30@gmail.com>
2024-02-10 02:41:40 -0300

    fix: avatar url from google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -19,7 +19,7 @@ export function buildNextAuthOptions(
             access_type: 'offline',
             response_type: 'code',
             scope:
-              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },
         },
         profile: (profile: GoogleProfile) => {
